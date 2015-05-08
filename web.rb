241edb807e531443bf2fd193c3fadc9e00cd09b2
Giordan Escalona <Gomputer@Giordans-Air.home>
2015-05-07 21:24:40 -0400

    First Commit?

--- web.rb
+++ web.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
 
 get '/' do
-  "Hello, world.   MY NAME IS GIORDAN AND I HAVE CHANGED THE TEXT"
+  "Hello, world.   MY NAME IS GIORDAN AND I HAVE CHANGED THE TEXT I CHANGED IT THE SECOND TIMW"
 end
